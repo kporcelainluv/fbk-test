aa9bde5b93d79d4cac289c9b0fe6d82333709eeb
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-03-17 18:04:12 +0300

    add Icon component

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,10 +1,15 @@
 import React from "react";
 import icons from "../img/icons/sprite.svg";
+import styled from "styled-components";
+
+const Svg = styled.svg`
+  margin: 0 10px;
+`;
 
 export const Icon = props => {
   const { height, width, name } = props;
   return (
-    <svg
+    <Svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       height={height}
@@ -12,6 +17,6 @@ export const Icon = props => {
       width={width}
     >
       <use xlinkHref={`${icons}#${name}`} />
-    </svg>
+    </Svg>
   );
 };
