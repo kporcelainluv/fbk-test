5e3f4c6f7ba014e93f32538150745ee7d4401c03
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-03-17 13:20:31 +0300

    wip on header menu

--- src/App.js
+++ src/App.js
@@ -3,17 +3,52 @@ import styled from "styled-components";
 
 import { Theme } from "./Theme";
 import GlobalFonts from "./fonts/fonts";
+import { Navigation } from "./components/navigation";
+import { Icon } from "./components/Icon";
 
-const H = styled.h1`
-  font-family: "CoFo Sans";
-  color: ${props => `${props.theme.colors.red}`};
+import { navigation } from "./consts";
+
+const MOBILE = "768px";
+const TABLET = "992px";
+const DESKTOP = "1200px";
+
+const Container = styled.div`
+  box-sizing: border-box;
+  max-width: 768px;
+  font-family: "Cofo Sans";
+  @media (min-width: ${TABLET}) {
+    max-width: 992px;
+  }
+  @media (min-width: ${DESKTOP}) {
+    max-width: 1200px;
+  }
+`;
+
+const HeaderBar = styled.div`
+  color: green;
 `;
 
 export const App = () => {
   return (
     <Theme>
       <GlobalFonts />
-      <H>Hello world</H>
+      <div style={{ display: "none" }}>
+        <include src="./img/icons/sprite.svg" />
+      </div>
+      <Container>
+        <header>
+          <nav>
+            <Navigation elements={navigation} />
+          </nav>
+          <HeaderBar>
+            <button>
+              <span>Открыть меню</span>
+            </button>
+            <Icon name="link" height="23" width="22" />
+            <Icon name="rouble" height="24" width="24" />
+          </HeaderBar>
+        </header>
+      </Container>
     </Theme>
   );
 };
