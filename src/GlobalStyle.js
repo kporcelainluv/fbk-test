dd7e9a7d7ac660f83ab68f8e9de42389d7a4e6ce
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-03-17 18:03:32 +0300

    add global style component

---
+++ src/GlobalStyle.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+import cofoRegular from "./fonts/CoFoSans-Regular.woff";
+import cofoRegular2 from "./fonts/CoFoSans-Regular.woff2";
+import cofoBold from "./fonts/CoFoSans-Bold.woff";
+import cofoBold2 from "./fonts/CoFoSans-Bold.woff2";
+
+export default createGlobalStyle`
+    * {
+    box-sizing: border-box
+    }
+    @font-face {
+    font-family: "CoFo Sans";
+    font-weight: normal;
+    src: url(${cofoRegular}) format("woff2"),
+      url(${cofoRegular2}) format("woff");
+  }
+  @font-face {
+    font-family: "CoFo Sans Bold";
+    font-weight: bold;
+    src: url(${cofoBold}) format("woff2"), url(${cofoBold2}) format("woff");
+  }
+  .visually-hidden {
+  position: absolute;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  width: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
+}
+
+`;
