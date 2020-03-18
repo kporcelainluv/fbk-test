03ac4bc44332fe00a0257a0ae1cd9dd0c04abcfe
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-03-18 20:51:12 +0300

    refactor social buttons

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,44 +1,43 @@
 import React from "react";
-import { Icon } from "./Icon";
 import styled from "styled-components";
+
+import { Icon } from "./Icon";
 
 const SocialList = styled.ul`
   display: flex;
-  padding-left: 0;
-  margin: 12px 0 0;
+  padding: 0;
+  margin: 0;
 
   li {
     list-style-type: none;
     margin-right: 12px;
+    display: inline-flex;
   }
-
-  @media (min-width: ${props => props.theme.breakPoints.tablet}) {
-    margin-top: 25px;
-  }
-  li {
-    margin-right: ${props => (props.margin === "small" ? "12px" : "16px")};
-  }
+  //
+  // @media (min-width: ${props => props.theme.breakPoints.tablet}) {
+  //   margin-top: 25px;
+  // }
 `;
 
-export const Socials = ({ margin = "regular" }) => {
+export const socials = [
+  "odnoklassniki",
+  "facebook",
+  "twitter",
+  "youtube",
+  "vk"
+];
+
+export const Socials = () => {
   return (
-    <SocialList margin={margin}>
-      <li>
-        <Icon name="odnoklassniki" width="32" height="33" viewBox="0 0 32 33" />
-      </li>
-      <li>
-        <Icon name="facebook" width="32" height="33" viewBox="0 0 32 33" />
-      </li>
-      <li>
-        {" "}
-        <Icon name="twitter" width="32" height="33" viewBox="0 0 32 33" />
-      </li>
-      <li>
-        <Icon name="youtube" width="32" height="33" viewBox="0 0 32 33" />
-      </li>
-      <li>
-        <Icon name="vk" width="32" height="33" viewBox="0 0 32 33" />
-      </li>
+    <SocialList>
+      {socials.map(social => {
+        return (
+          <li key={social}>
+            <Icon name={social} width="32" height="32" viewBox="0 0 32 32" />
+            <span className="visually-hidden">{social}</span>
+          </li>
+        );
+      })}
     </SocialList>
   );
 };
