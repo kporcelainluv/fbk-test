69a308f379099913fae1f244d9f34632bc3019f0
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-03-18 20:45:50 +0300

    refactor Icon

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,16 +1,14 @@
 import React from "react";
 import icons from "../img/icons/sprite.svg";
 
-export const Icon = props => {
-  const { height, width, name, viewbox } = props;
-
+export const Icon = ({ height, width, name, viewbox }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       height={height}
-      viewBox={viewbox ? viewbox : `0 0 ${height} ${width}`}
       width={width}
+      viewBox={viewbox ? viewbox : `0 0 ${height} ${width}`}
     >
       <use xlinkHref={`${icons}#${name}`} />
     </svg>
