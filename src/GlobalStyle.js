9c30f179c68ae3fa12603261be244a7e0995000f
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-03-19 09:29:18 +0300

    transfer font to public

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,10 +1,5 @@
 import { createGlobalStyle } from "styled-components";
 
-import cofoRegular from "./fonts/CoFoSans-Regular.woff";
-import cofoRegular2 from "./fonts/CoFoSans-Regular.woff2";
-import cofoBold from "./fonts/CoFoSans-Bold.woff";
-import cofoBold2 from "./fonts/CoFoSans-Bold.woff2";
-
 export default createGlobalStyle`
   * {
     box-sizing: border-box
@@ -13,14 +8,15 @@ export default createGlobalStyle`
   @font-face {
     font-family: "CoFo Sans";
     font-weight: normal;
-    src: url(${cofoRegular}) format("woff2"),
-      url(${cofoRegular2}) format("woff");
+    src: url('./fonts/CoFoSans-Regular.woff2') format("woff2"),
+      url('./fonts/CoFoSans-Regular.woff') format("woff");
   }
   
   @font-face {
     font-family: "CoFo Sans Bold";
     font-weight: bold;
-    src: url(${cofoBold}) format("woff2"), url(${cofoBold2}) format("woff");
+    src: url("./fonts/CoFoSans-Bold.woff2") format("woff2"), 
+    url("./fonts/CoFoSans-Bold.woff") format("woff");
   }
   
   .visually-hidden {
