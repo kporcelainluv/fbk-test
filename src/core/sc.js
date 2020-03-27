e0a4dc3f4fc99ce50239be969e3b423c62cc3171
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-03-27 13:03:39 +0300

    add font-family and breakpoints consts

--- src/core/sc.js
+++ src/core/sc.js
@@ -1,3 +1,10 @@
 export const breakpoint = {
-  tablet: p => p.theme.breakPoints.tablet
+  tablet: p => p.theme.breakPoints.tablet,
+  smallDesktop: p => p.theme.breakPoints.smallDesktop,
+  desktop: p => p.theme.breakPoints.desktop
+};
+
+export const fontFamily = {
+  regular: "CoFo Sans, Arial, sans-serif",
+  bold: "CoFo Sans Bold, Arial, sans-serif"
 };
